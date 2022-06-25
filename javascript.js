203d112cc187cd5537b2790d926e5d073a9049a8
Juan Correa <juancorrea50@yahoo.com>
2022-06-25 17:53:44 -0500

    Approve test for gameplay functionality
    The core function of the gameplay has been tested and works.

--- javascript.js
+++ javascript.js
@@ -1,36 +1,63 @@
 /*Look at TODO list in README and TOP assignment for directions*/
 
 /*Make variables for game*/
+let playerChoice;
 let playerScore=0;
 let opponentScore=0;
 
 /*Computer Play*/
 function computerPlay(){
-    return "scissors"
+    let x;
+    x=Math.floor(Math.random()*(3-1+1))+1;
+
+    switch(x){
+        case 1:
+            return "Rock";
+        break;
+        case 2:
+            return "Paper";
+        break;
+        case 3:
+            return "Scissors";
+        break;
+        default:
+            return "Option not chosen(bug)"
+        
+    }
 }
-console.log("Opponent has chosen "+computerPlay())
+
 
 /*Game Function */
 function play(playerSelection, opponentSelection){
-        if(playerSelection.toLowerCase() === "rock" && opponentSelection.toLowerCase()==="scissors" 
-        || playerSelection.toLowerCase()==="scissors" && opponentSelection.toLowerCase()==="paper" || 
-        playerSelection.toLowerCase()==="paper" && opponentSelection.toLowerCase()==="rock"){
+        console.log(playerSelection + " " + opponentSelection);
+        /*Win Condition */
+        if(playerSelection.toLowerCase() == "rock" && opponentSelection.toLowerCase()=="scissors" 
+        || playerSelection.toLowerCase()==="scissors" && opponentSelection.toLowerCase()=="paper" || 
+        playerSelection.toLowerCase()=="paper" && opponentSelection.toLowerCase()=="rock"){
             playerScore++;
             return "Player has won this round!!!";
-        } else if(playerSelection.toLowerCase() === "scissors" && opponentSelection.toLowerCase() ==="rock" 
-        || playerSelection.toLowerCase()==="paper" && opponentSelection.toLowerCase()==="scissors" || 
-        playerSelection.toLowerCase()==="rock" && opponentSelection.toLowerCase()==="paper"){
+        }
+        /*Lose Condition */
+        else if(playerSelection.toLowerCase() == "scissors" && opponentSelection.toLowerCase() =="rock" 
+        || playerSelection.toLowerCase()=="paper" && opponentSelection.toLowerCase()=="scissors" || 
+        playerSelection.toLowerCase()=="rock" && opponentSelection.toLowerCase()=="paper"){
             opponentScore++;
             return "Player has lost this round...";
-        } else{
+        }
+        /*Draw Condition */
+        else if(playerSelection.toLowerCase() === opponentSelection.toLowerCase()){
             return "Player and opponent have reached a draw no point is given"
         }
 }
 
-console.log(play("rock", computerPlay()));
+console.log(play("ROCK", computerPlay()));
 
-
-/*For loop on win condition*/
-for(x=0;x<5;x++){
-
+/*Game Function*/
+function game(){
+    /*For loop for 5 games*/
+    for(x=0;x<=5;x++){
+        
+    }
 }
+
+
