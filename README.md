d4207826d5acb41eb8ef36c55c43ab4e8d3f5320
Juan Correa <juancorrea50@yahoo.com>
2022-06-25 17:07:04 -0500

    Modify TODO list and js file to include play() function
    TODO list contains objectives more in line to the assignment directive.
    play function added to javascript file using a switch statement. Still
    not tested for functionality.

--- README.md
+++ README.md
@@ -3,11 +3,11 @@ Description: Play Rock, Paper, Scissors with a simple AI and determine the victo
 
 
 TO-DO:
--Set up Variables
--Make AI choose between the three options
--Loop gameplay until victor is chosen
--Ensure the game does end and succesfully close loop
--Test output of functions to determine functionality
+-Set up Variables (v/)
+-Make AI choose between the three options (Defaulted to Scissors)
+-Loop gameplay until victor is chosen()
+-Ensure the game does end and succesfully close loop()
+-Test functionality()
 
 
 
--- javascript.js
+++ javascript.js
@@ -1,1 +1,38 @@
 /*Look at TODO list in README and TOP assignment for directions*/
+
+/*Make variables for game*/
+let playerScore=0;
+let opponentScore=0;
+
+/*Computer Play*/
+function computerPlay(){
+    return "Scissors"
+}
+console.log("Opponent has chosen "+computerPlay())
+
+/*Game Function */
+function play(playerSelection,opponentSelection){
+        switch(playerSelection, opponentSelection){
+            case playerSelection.toLowerCase() == "rock" && opponentSelection.toLowerCase() =="scissors" 
+            || playerSelection.toLowerCase()=="scissors" && opponentSelection.toLowerCase()=="paper" || 
+            playerSelection.toLowerCase()=="paper" && opponentSelection.toLowerCase()=="rock":
+                playerScore++;
+                return "Player has won this round!!!";
+            break;
+            case playerSelection.toLowerCase() == "scissors" && opponentSelection.toLowerCase() =="rock" 
+            || playerSelection.toLowerCase()=="paper" && opponentSelection.toLowerCase()=="scissors" || 
+            playerSelection.toLowerCase()=="rock" && opponentSelection.toLowerCase()=="paper":
+                opponentScore++;
+                return "Player has lost this round...";
+            break;
+            default:
+                return "Player and opponent have reached a draw no point is given"
+            
+        }
+}
+
+
+/*For loop on win condition*/
+for(x=0;x<5;x++){
+
+}
