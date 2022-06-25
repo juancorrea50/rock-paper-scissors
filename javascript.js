5c169def6cab97406f87aae56478ac5846e67a89
Juan Correa <juancorrea50@yahoo.com>
2022-06-25 17:24:02 -0500

    Change switch statement to if/else statement for gameplay

--- javascript.js
+++ javascript.js
@@ -6,30 +6,28 @@ let opponentScore=0;
 
 /*Computer Play*/
 function computerPlay(){
-    return "Scissors"
+    return "scissors"
 }
 console.log("Opponent has chosen "+computerPlay())
 
 /*Game Function */
-function play(playerSelection,opponentSelection){
-        switch(playerSelection, opponentSelection){
-            case playerSelection.toLowerCase() == "rock" && opponentSelection.toLowerCase() =="scissors" 
-            || playerSelection.toLowerCase()=="scissors" && opponentSelection.toLowerCase()=="paper" || 
-            playerSelection.toLowerCase()=="paper" && opponentSelection.toLowerCase()=="rock":
-                playerScore++;
-                return "Player has won this round!!!";
-            break;
-            case playerSelection.toLowerCase() == "scissors" && opponentSelection.toLowerCase() =="rock" 
-            || playerSelection.toLowerCase()=="paper" && opponentSelection.toLowerCase()=="scissors" || 
-            playerSelection.toLowerCase()=="rock" && opponentSelection.toLowerCase()=="paper":
-                opponentScore++;
-                return "Player has lost this round...";
-            break;
-            default:
-                return "Player and opponent have reached a draw no point is given"
-            
+function play(playerSelection, opponentSelection){
+        if(playerSelection.toLowerCase() === "rock" && opponentSelection.toLowerCase()==="scissors" 
+        || playerSelection.toLowerCase()==="scissors" && opponentSelection.toLowerCase()==="paper" || 
+        playerSelection.toLowerCase()==="paper" && opponentSelection.toLowerCase()==="rock"){
+            playerScore++;
+            return "Player has won this round!!!";
+        } else if(playerSelection.toLowerCase() === "scissors" && opponentSelection.toLowerCase() ==="rock" 
+        || playerSelection.toLowerCase()==="paper" && opponentSelection.toLowerCase()==="scissors" || 
+        playerSelection.toLowerCase()==="rock" && opponentSelection.toLowerCase()==="paper"){
+            opponentScore++;
+            return "Player has lost this round...";
+        } else{
+            return "Player and opponent have reached a draw no point is given"
         }
 }
+
+console.log(play("rock", computerPlay()));
 
 
 /*For loop on win condition*/
